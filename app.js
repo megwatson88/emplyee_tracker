4144cf6e47a9de9c20a0e0cfcd9193e0f85bb119
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-09-05 18:57:43 -0700

    functions additions

--- app.js
+++ app.js
@@ -1,56 +1,107 @@
+const { response } = require('express');
 const express = require('express');
 const inquirer = require('inquirer');
+const mysql = require('mysql');
+const Connection = require('mysql2/typings/mysql/lib/Connection');
 
-const PORT = process.env.PORT || 3001 
+
+
+const PORT = process.env.PORT || 3001
 const app = express();
 
 //express middlewear 
-app.use(express.urlencoded({extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
 //test 
-app.get('/', (req, res)=>{
-    res.json({
-        message: 'hello world'
-    });
-});
+// app.get('/', (req, res)=>{
+//     res.json({
+//         message: 'hello world'
+//     });
+// });
+
+
 // inquirer employee questions
-inquirer
-    .prompt([
-        {
-            type: input,
-            name: first_name,
-            message: "What is the employee's first name?"
-        },
-        {
-            type: input,
-            name: last_name,
-            message: "What is the employee's last name?"
-        },
-        {
-            type: input,
-            name: role,
-            message: "What is the employee's role?"
-        },
-        {
-            type: list,
-            name: department,
-            message: "What department is this employee in?",
-            choices: ['Leadership', 'Marketing', 'Sales', 'HR', 'Finance']
-        },
-        {
-            type: input,
-            name: salary,
-            message: "What is the employee's salary?"
-        },
-        {
-            type: input,
-            name: last_name,
-            message: "What is the employee's last name?"
-        },
-        ])
-    .then
+
+
+const startApp = () => {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Welcome to your employee tracking system! Would you like to VIEW, ADD or UPDATE an employee?",
+                choices: ["VIEW", "ADD", "UPDATE", "END"]
+            }
+
+                .then((response) => {
+
+                    //if chooses view 
+                    if (response.chooseView === "VIEW") {
+                        inquirer
+                            .prompt ({
+                                name: "pickedView",
+                                type: "list",
+                                choices: ["Departments", "Roles", "Employees"]
+                            })
+                            .then((response)=>{
+
+                            })
+                    }
+
+                }
+                )
+        ]
+        )
+}
+//         {
+//             type: input,
+//             name: first_name,
+//             message: "What is the employee's first name?"
+//         },
+//         {
+//             type: input,
+//             name: last_name,
+//             message: "What is the employee's last name?"
+//         },
+//         {
+//             type: input,
+//             name: role,
+//             message: "What is the employee's role?"
+//         },
+//         {
+//             type: list,
+//             name: department,
+//             message: "What department is this employee in?",
+//             choices: ['Leadership', 'Marketing', 'Sales', 'HR', 'Finance']
+//         },
+//         {
+//             type: input,
+//             name: salary,
+//             message: "What is the employee's salary?"
+//         },
+//         {
+//             type: input,
+//             name: last_name,
+//             message: "What is the employee's last name?"
+//         },
+//         {
+//             type: input,
+//             name: manager_name,
+//             message: "Who is the manager?"
+//         },
+//         {
+//             type: input,
+//             name: manager_id,
+//             message: "What is the manager's id?"
+//         },
+//         {
+//             type: input,
+//             name: last_name,
+//             message: "What is the employee's last name?"
+//         },
+// ])}
+//     //.then
